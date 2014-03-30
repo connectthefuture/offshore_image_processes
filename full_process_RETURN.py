9c243a9fb6cb55273a3610f9784967944fb18c45
jb <relic7@aol.com>
2014-03-30 02:52:55 -0400

    zeros out

--- full_process_RETURN.py
+++ full_process_RETURN.py
@@ -7,21 +7,43 @@ import zipfile,sys,datetime,os,re
 
 
 #####################################################################################################################
+#
+#def unzip_dir_savefiles(zipin):
+#    zipin  = os.path.abspath(zipin)
+#    zipdir = '/'.join(zipin.split('/')[:-1])
+#    #print zipdir
+#    # Open zip file
+#    zipf   = zipfile.ZipFile(zipin, 'r')
+#    # ZipFile.read returns the bytes contained in named file
+#    filenames = zipf.namelist()
+#    #print filenames
+#    for filename in filenames:
+#        f = zipf.open(filename)
+#        contents = f.read()
+#        #writefile = os.path.join(zipdir,filename)
+#        writefile = os.path.join(zipdir, filename.split('/')[0])        
+#        print writefile, "NO", zipdir + filename.split('/')[0] + '.zip'
+#        with open(writefile, 'w') as wfile:
+#            wfile.write(contents)
 
 def unzip_dir_savefiles(zipin):
-    zipin  = os.path.abspath(zipin)
+    #zipin  = os.path.abspath(sys.argv[1])
     zipdir = '/'.join(zipin.split('/')[:-1])
-    # print zipdir
+    print zipdir
     # Open zip file
+    #zipname  = os.path.join(zipdir, filename)
     zipf   = zipfile.ZipFile(zipin, 'r')
+
     # ZipFile.read returns the bytes contained in named file
     filenames = zipf.namelist()
+
     for filename in filenames:
         print 'Processing {0}'.format(filename)
         f = zipf.open(filename)
         contents = f.read()
         writefile = os.path.join(zipdir,filename)
-        with open(writefile, 'w') as wfile:
+        zipname  = os.path.join(zipdir, filename)
+        with open(zipname, 'w') as wfile:
             wfile.write(contents)
     return zipin
 
@@ -60,7 +82,7 @@ for file in files:
     parentdir   = file.split('/')[-1]
     downloaddir = os.path.join(fullftp, parentdir)
     ##TODO: download remote zip via ftp then unzip_dir_savefiles(zipin)
-    print downloaddir
+    #print downloaddir
 
 #####################################################################################################################
 ##### Unzip downloaded file##########################################################################################
@@ -70,8 +92,8 @@ regex_zipfilename = re.compile(r'^[^\.].+?[zipZIP]{3}$')
 regex_zipfilepath = re.compile(r'^/.+?[zipZIP]{3}$')
 
 todaysdate = str(datetime.date.today())
-
-
+#
+#
 #colorstyle = filepath.split('/')[-1][:9]
 #if re.findall(regex_colorstyle, colorstyle):
 ftp = ftplib.FTP(ftpurl)
@@ -99,23 +121,27 @@ returndir = '/Users/JCut/Dropbox/DEVROOT/srv/media/Post_Complete/Complete_Archiv
 for filename in filenames:
     local_filename = os.path.join(returndir,filename)
     file = open(local_filename, 'wb')
-    ftp.retrbinary('RETR '+ filename, file.write)
-
+    #ftp.retrbinary('RETR '+ filename, file.write)
     file.close()
-
 ftp.close()
 
 
 ######### find out how many zips dloaded then extract pngs from zip
-zipfiles_returned = []
-for z in os.listdir(returndir):
-    if re.findall(regex_zipfilepath, z):
-        zipfiles_returned.append(z)
+import glob
 
-if len(zipfiles_returned) == 1:
+zipfiles_returned = []
+for z in glob.glob(os.path.join(returndir, '*.zip')):
+    #print os.path.abspath(z)
+    #if re.findall(regex_zipfilepath, z):
+    zipfiles_returned.append(z)
+
+cc='/Users/JCut/Dropbox/DEVROOT/srv/media/Post_Complete/Complete_Archive/SendReceive_BGRemoval/2_Returned/batch_2014-03-29.zip'
+print zipfiles_returned
+if len(zipfiles_returned) == 2:
     zipreturned = zipfiles_returned.pop()
     unzip_dir_savefiles(zipreturned)
 else:
-    for z in zipreturned:
+    for z in zipfiles_returned:
+        print z
         unzip_dir_savefiles(z)
 
