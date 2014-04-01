84e9e798c8c693da506c401ac296a3942c09c090
johnb <john.bragato@bluefly.com>
2014-04-01 19:49:58 -0400

    c

--- full_process_SEND.py
+++ full_process_SEND.py
@@ -58,7 +58,7 @@ def getbinary_ftp_netsrv101(remote_pathtofile, outfile=None):
     if outfile is None:
         outfile = sys.stdout
     destfile = open(outfile, "wb")
-    ftpdown.retrbinary("RETR " + remote_pathtofile, destfile.write, 8*1024)
+    ftpdown.retrbinary("RETR " + remote_pathtofile, destfile.write)  #, 8*1024)
     destfile.close()
 
 ###
