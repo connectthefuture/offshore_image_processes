caa150fef24705e53979eca6756ea639d50f77b3
johnb <john.bragato@bluefly.com>
2014-04-01 16:05:59 -0400

    x

--- full_process_RETURN.py
+++ full_process_RETURN.py
@@ -119,7 +119,7 @@ def subproc_pad_to_x480(file,destdir):
         '-colorspace',
         'sRGB',
         '-unsharp',
-        '2x1.3', 
+        '.5x1.3', 
         '-quality',
         '100',
         #'-strip', 
