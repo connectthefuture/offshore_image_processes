291b7b2793af42ac34451ce39778e7e65b569567
johnb <john.bragato@bluefly.com>
2014-04-01 21:12:13 -0400

    x

--- full_process_RETURN.py
+++ full_process_RETURN.py
@@ -164,6 +164,47 @@ def sqlQuery_500_set_returndt(style):
         print "Duplicate Entry {0}".format(style)
     connection.close()
 #####################################################################################################################
+## 8 ## Clear List page cdn with edgecast and style number, no version needed on List page for now
+##
+def edgecast_clear_listpage_only(colorstyle):
+    import pycurl,json,sys,os
+    token = "9af6d09a-1250-4766-85bd-29cebf1c984f"
+    account = "4936"
+    mediaPath = "http://cdn.is.bluefly.com/mgen/Bluefly/prodImage.ms?productCode={0}&width=251&height=300".format(colorstyle)
+    mediaType = "8"
+    purgeURL = "https://api.edgecast.com/v2/mcc/customers/{0}/edge/purge".format(account)
+    if token != "" and account != "" and mediaPath != "" and mediaType != "":
+        ## Create send data
+        data = json.dumps({
+        'MediaPath' : mediaPath,
+        'MediaType' : mediaType 
+        })
+        #data = json_encode(request_params)
+        head_authtoken = "Authorization: tok:{0}".format(token)
+        head_content_len= "Content-length: {0}".format(str(len(data)))
+        head_accept = 'Accept: application/json'
+        head_contenttype = 'Content-Type: application/json'
+
+        c = pycurl.Curl()
+        c.setopt(pycurl.URL, purgeURL)
+        c.setopt(pycurl.PORT , 443)
+        c.setopt(pycurl.SSL_VERIFYPEER, 0)
+        c.setopt(pycurl.HEADER, 0)
+        #c.setopt(pycurl.INFOTYPE_HEADER_OUT, 1)
+        #c.setopt(pycurl.RETURNTRANSFER, 1)
+        c.setopt(pycurl.FORBID_REUSE, 1)
+        c.setopt(pycurl.FRESH_CONNECT, 1)
+        c.setopt(pycurl.CUSTOMREQUEST, "PUT")
+        c.setopt(pycurl.POSTFIELDS,data)
+        c.setopt(pycurl.HTTPHEADER, [head_authtoken, head_contenttype, head_accept, head_content_len])
+        try:
+            c.perform()
+            c.close()
+            print "Successfully Sent Purge Request for --> {0}".format(mediaPath)
+        except pycurl.error, error:
+            errno, errstr = error
+            print 'An error occurred: ', errstr
+
 #####################################################################################################################
 ## RUN ##
 ######### find out how many zips dloaded then extract pngs from zip
@@ -303,3 +344,7 @@ for f in glob.glob(os.path.join(archdir, '*/*_LP.png')):
         print "Failed Entrering Return dt for --> {0}".format(colorstyle)
     #####################################################
     #####################################################
+    try:
+        edgecast_clear_listpage_only(colorstyle)
+    except:
+        pass
