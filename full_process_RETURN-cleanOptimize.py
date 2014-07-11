99f654e5f399b8ca3b27c93e90fe757225912db8
johnb <jbragato@yahoo.com>
2014-07-11 19:18:31 -0400

    x

--- full_process_RETURN-cleanOptimize.py
+++ full_process_RETURN-cleanOptimize.py
@@ -768,7 +768,8 @@ import time
 start_time = time.time()
 main()
 end_time = time.time() - start_time
-print("Total--- %s(end_time) seconds ---" % end_time)#
+print("Total--- %s(end_time) seconds ---" % end_time)
+#
 
 # if __name__ == '__main__':
 #     import timeit
